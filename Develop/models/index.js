3fb8452642d3d2cefa2bea57d87e5277dadafd40
Jake Preciado <jacobpreciado@gmail.com>
2023-10-23 22:07:16 -0600

    Final commit

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,26 +4,22 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'product_id',
   unique: false
 });
 
-// Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'product_id',
   onDelete: 'CASCADE'
 });
 
-// Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: { model: ProductTag },
   foreignKey: 'product_id',
   onDelete: 'CASCADE'
 });
 
-// Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: { model: ProductTag },
   foreignKey: 'tag_id',
